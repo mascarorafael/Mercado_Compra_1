c18f6bfd609be6858419bf0e425bbf0380955f4f
Kaio Vinicius <vinciuskaio49@gmail.com>
2024-08-06 16:17:14 -0300

    Implementações de visibilidade navbar part 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+  before_action :authenticate_user!
+  before_action :set_devise_variables
+
+  private
+
+  def set_devise_variables
+    if user_signed_in?
+      @resource = User.new
+      @resource_name = :user
+    else
+      @resource_name = :user
+      @resource = User.new
+    end
+  end
 end
