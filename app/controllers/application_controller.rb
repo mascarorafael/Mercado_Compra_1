d9786bd41ce8411e7bccf508eb474dd16bb72863
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <rafa.mascaro@gmail.com>
2024-12-13 16:24:57 -0300

    alterações

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :set_devise_variables
-
   def after_sign_in_path_for(resource)
     products_path
   end
-
   def after_sign_out_path_for(resource_or_scope)
     new_user_session_path
   end
 
   private
-
   def set_devise_variables
     @resource = User.new
     @resource_name = :user
